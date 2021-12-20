30473d8baa2429758de9a7f0b7f31e1f0f9d4c92
Denis ALeksandrov <denis_aleksandrov_12@mail.ru>
2021-12-21 00:31:58 +0300

    Add project files.

---
+++ src/Web/Startup.cs
@@ -0,0 +1,15 @@
+﻿namespace Web
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+        }
+    }
+}
